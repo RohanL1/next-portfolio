07673125dd07cd5faf330551e3ee86dd50c49f47
Rohan Lagare <rohan.v.lagare@gmail.com>
2023-08-14 16:14:38 -0700

    for more github link

--- app/page.js
+++ app/page.js
@@ -9,11 +9,11 @@ import web from '../public/code.png'
 import data_eng from '../public/data_eng.png'
 import ai_logo from '../public/ai.png'
 import web1 from '../public/face_rec.png'
-import web2 from '../public/web2.png'
-import web3 from '../public/web3.png'
-import web4 from '../public/web4.png'
-import web5 from '../public/web5.png'
-import web6 from '../public/web6.png'
+import web2 from '../public/formore.png'
+import web3 from '../public/formore.png'
+import web4 from '../public/formore.png'
+import web5 from '../public/formore.png'
+import web6 from '../public/formore.png'
 // import resume from '/Rohan_Lagare_Resume.pdf'
 
 // import { useState } from 'react'
@@ -144,20 +144,24 @@ export default function Home() {
           </div>
           
           <div className='basis-1/3 flex-1 '>
+          <a href="https://github.com/RohanL1/" target="_blank">
             <Image src={web2} alt='web1 image' className='rounded-lg object-cover' width={"100%"} height={"100%"} layout='responsive'/>
+          </a>
           </div>
+
           <div className='basis-1/3 flex-1 '>
+          <a href="https://github.com/RohanL1/" target="_blank">
             <Image src={web3} alt='web1 image' className='rounded-lg object-cover' width={"100%"} height={"100%"} layout='responsive'/>
+          </a>
           </div>
+
           <div className='basis-1/3 flex-1 '>
+          <a href="https://github.com/RohanL1/" target="_blank">
             <Image src={web4} alt='web1 image' className='rounded-lg object-cover' width={"100%"} height={"100%"} layout='responsive'/>
+          </a>
           </div>
-          <div className='basis-1/3 flex-1 '>
-            <Image src={web5} alt='web1 image' className='rounded-lg object-cover' width={"100%"} height={"100%"} layout='responsive'/>
-          </div>
-          <div className='basis-1/3 flex-1 '>
-            <Image src={web6} alt='web1 image' className='rounded-lg object-cover' width={"100%"} height={"100%"} layout='responsive'/>
-          </div>
+
+
         </div>
       </section>
     
